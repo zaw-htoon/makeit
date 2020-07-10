5a8a9e348dd48d4425a5f64d710101783bc2caa1
zaw <zahton@gmail.com>
2020-07-10 16:48:07 +0800

    skey removed

--- profiles_proj/settings.py
+++ profiles_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c1guifdg1bl@^p9qbl7km$!=!a%_&-eq1*^*tal-ffd=m)0ck5'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
